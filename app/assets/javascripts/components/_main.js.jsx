236cccb4e3c4a95447bc6181a8ee85a07952c13c
Jack Pincus <jwpincus@gmail.com>
2019-01-21 10:39:28 -0800

    controllers for User API and root created

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,7 @@
+const Main = (props) => {
+  return (
+    <div>
+      <h1>Article Manager</h1>
+    </div>
+  )
+}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session #No longer default in Rails 5+. Allows any request to ignore CSRF token authenticity for API purposes
 end
